717ec846d3ef8d22ac9e1a16987180d3bbd6dd56
yepengxj <yepeng_xj@163.com>
2016-09-27 13:04:35 +0800

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,6 @@ kinit $BSI_HDFS_HDFSDEMO_USERNAME <<!!
 $BSI_HDFS_HDFSDEMO_PASSWORD
 !!
 
-echo "36.110.131.65 hadoop-1.jcloud.local" >> /etc/hosts
-echo "36.110.132.55 hadoop-2.jcloud.local" >> /etc/hosts
 echo "start main"
 date
 tail -f /dev/null
