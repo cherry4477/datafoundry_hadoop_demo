ff039d71ff1670a38736f738d012fc34c6685f86
yepengxj <yepeng_xj@163.com>
2016-09-27 12:36:40 +0800

    modified:   Dockerfile  	new file:   krb5.conf  	modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 echo "kinit"
 date
-kinit $(BSI_HDFS_HDFSDEMO_USERNAME)@ASIAINFO.COM <<!!
-$(BSI_HDFS_HDFSDEMO_PASSWORD)
+kinit $BSI_HDFS_HDFSDEMO_USERNAME@ASIAINFO.COM <<!!
+$BSI_HDFS_HDFSDEMO_PASSWORD
 !!
 
+echo "36.110.131.65 hadoop-1.jcloud.local" >> /etc/hosts
+echo "36.110.132.55 hadoop-2.jcloud.local" >> /etc/hosts
 echo "start main"
 date
 tail -f /dev/null
